faefc879dfd8d7e112969bb659b845af429f205b
Luke Benstead <Luke Benstead kazade@gmail.com>
2013-03-27 10:47:01 +0000

    Read the SP and PC initial values from memory on boot

--- main.cpp
+++ main.cpp
@@ -5,20 +5,42 @@ using namespace std;
 
 class RAM {
 public:
-    RAM(uint32_t size);
+    RAM(uint32_t size) {
+        bytes_.resize(size);
+    }
 
-    uint8_t* read(uint32_t address);
-    void write(uint32_t address, uint8_t* data);
+    template<typename T>
+    T read(uint32_t address) {
+        assert(address >= 0 && address < bytes_.size());
+        if(!check_read_address(address)) {
+            return 0;
+        }
+        return *((T*)&bytes_[address]);
+    }
 
+    template<typename T>
+    void write(uint32_t address, T data) {
+        assert(address >= 0 && address < bytes_.size());
+        if(!check_write_address(address)) {
+            return;
+        }
+        //Cast to pointer of type T then dereference to assign the value
+        *((T*)&bytes_[address]) = data;
+    }
+
+    bool load_from_disk(const std::string& filename) {
+
+    }
 private:
     virtual bool check_write_address(uint32_t address) { return true; }
     virtual bool check_read_address(uint32_t address) { return true; }
-    std::vector<uint8_t> bytes;
+    std::vector<uint8_t> bytes_;
 };
 
 class ROM : public RAM {
 public:
-    ROM(uint32_t size);
+    ROM(uint32_t size):
+        RAM(size) {}
 
 private:
     bool check_write_address(uint32_t address) { return false; }
@@ -26,13 +48,23 @@ private:
 
 class MemoryMap {
 public:
-    void add_range(uint32_t low, uint32_t high, RAM* block, uint32_t offset=0);
-
-    void write(uint32_t address, uint8_t* data) {
-
+    void add_range(uint32_t low, uint32_t high, RAM* block, uint32_t offset=0) {
+        ranges_[std::make_pair(low, high)] = std::make_pair(block, offset);
     }
 
-    uint8_t* read(uint32_t address) {
+    template<typename T>
+    void write(uint32_t address, T data) {
+        for(RangeMap::const_iterator it = ranges_.begin(); it != ranges_.end(); ++it) {
+            HighLow& range = (*it).first;
+            if(address >= range.first && range < range.second) {
+                RAMOffset& ram_offset = (*it).second;
+                return ram_offset.first->write(address + ram_offset.second, data);
+            }
+        }
+    }
+
+    template<typename T>
+    T read(uint32_t address) {
         for(RangeMap::const_iterator it = ranges_.begin(); it != ranges_.end(); ++it) {
             HighLow& range = (*it).first;
             if(address >= range.first && range < range.second) {
@@ -40,6 +72,8 @@ public:
                 return ram_offset.first->read(address + ram_offset.second);
             }
         }
+
+        return 0;
     }
 
 private:
@@ -105,10 +139,16 @@ public:
     M68K() {}
 
     void tick() {
-        uint32_t opcode = memory().read(program_counter_++);
+        //Is the opcode 32 bit?
+        uint32_t opcode = memory().read(PC.read());
+
         switch(opcode) {
 
         }
+
+        PC.write(PC.read() + 4); //Should this be 4?
+
+        //Handle interrupts
     }
 };
 
@@ -117,6 +157,17 @@ public:
     Z80();
 
     void tick();
+
+    Register<uint8_t> AF;
+    Register<uint16_t> BC;
+    Register<uint16_t> DE;
+    Register<uint16_t> HL;
+    Register<uint16_t> SP;
+
+    Register<uint16_t> IX;
+    Register<uint16_t> IY;
+    Register<uint8_t> I;
+    Register<uint8_t> R;
 };
 
 
@@ -124,10 +175,11 @@ int main() {
     M68K cpu;
     Z80 cpu2;
 
-    //ROM bios = ROM(2 * 1024); //BIOS
+    ROM tmss = ROM(2 * 1024); //BIOS
     ROM cartridge = ROM(4096 * 1024); //4M
     RAM m68k_ram = RAM(64 * 1024); //64k
     RAM z80_ram = RAM(8 * 1024); //8k
+    RAM vram = RAM(64 * 1024); //64k
 
     //FIXME: add mappings for mirrored data
     cpu.memory().add_range(0x000000, 0x3FFFFF, &cartridge);
@@ -137,13 +189,11 @@ int main() {
     cpu.memory().add_range(0xA00000, 0xA0FFFF, &z80_ram);
     cpu2.memory().add_range(0x000000, 0x00FFFF, &z80_ram);
 
-    uint32_t initial_stack_pointer = 0x00FFE000;
-    uint32_t start_of_rom = 0x00000200;
-
-    cpu.PC.write(start_of_rom);
-    cpu.SP.write(initial_stack_pointer); //Apparently the MD sets this as the default stack pointer
-
     cartridge.load_from_disk("./sonic1.bin");
+
+    //Build vector table...
+    cpu.SP.write(cpu.memory().read<uint32_t>(0x000000)); //First value is the stack pointer
+    cpu.PC.write(cpu.memory().read<uint32_t>(0x000004)); //4 bytes in is the address for the program counter
 
     while(true) {
         cpu.tick();
